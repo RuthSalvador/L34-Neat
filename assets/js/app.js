0abee8ad627e5d293a6ec408298950d9b310784e
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-05-23 16:50:05 -0500

    header, home

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+var close = document.getElementById("close");
+close.addEventListener("click", function(){
+  var alertNav = document.getElementById("alert-nav");
+  alertNav.style.display = "none";
+})
